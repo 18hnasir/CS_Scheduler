55bb0a61913918d123e9deece972446d0fe35d85
kai <38149120+kaivantaylor@users.noreply.github.com>
2021-04-15 09:17:43 -0700

    Update InputController.js

--- InputController.js
+++ InputController.js
@@ -3,10 +3,16 @@
  *
  * 
  * 
- * */
-
+ * 
+*/
 
 /* Import statements for functions from other js files */
 import { getPreReq } from './courseInfo.js';
 import { getCredits } from './courseInfo.js';
 import { generateSchedule } from './Scheduler.js';
+
+// Get all of the information form the user
+
+// Make an array and get the array of classes that were checked
+
+// Double array for the GTA
