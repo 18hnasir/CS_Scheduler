b005020cf8da59951f9facf910e965a251caa8b6
kai <38149120+kaivantaylor@users.noreply.github.com>
2021-04-15 09:36:26 -0700

    Update InputController.js

--- InputController.js
+++ InputController.js
@@ -1,8 +1,9 @@
 /* InputController.js
  * Purpose: 
  *
- * 
- * 
+ * Get all of the information form the user
+ * Make an array and get the array of classes that were checked
+ * Double array for the GTA
  * 
 */
 
@@ -11,8 +12,28 @@ import { getPreReq } from './courseInfo.js';
 import { getCredits } from './courseInfo.js';
 import { generateSchedule } from './Scheduler.js';
 
-// Get all of the information form the user
+// This function needs to return an array
+export function getInput(){
 
-// Make an array and get the array of classes that were checked
+    // Array that will hold all of the classes that were inputted
+    var myArray = []; 
 
-// Double array for the GTA
+    // Computer Science Core Check
+    if (document.getElementById("CS110").checked){
+        myArray.push("CS110");
+        alert("Pushed CS110 to returned myArray");
+    }
+
+    // Senior Computer Science Check
+
+    // Mathematics Check
+
+    // Natural Science Check
+
+    // Computer Science Related Check
+
+    // Communication and English Check
+
+
+
+}
